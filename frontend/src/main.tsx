0ffbc4964397bd481c4d590baa558ff0b3d59fe0
Tran Ngoc Anh <124139561+tnanh1407@users.noreply.github.com>
2026-07-09 00:02:31 +0700

    feat: add Vercel Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
 import router from './routes'
 import './index.css'
 import { HelmetProvider } from 'react-helmet-async'
@@ -27,5 +28,6 @@ createRoot(document.getElementById('root')!).render(
         </HelmetProvider>
       </GoogleOAuthProvider>
     </QueryClientProvider>
+    <Analytics />
   </StrictMode>,
 )
